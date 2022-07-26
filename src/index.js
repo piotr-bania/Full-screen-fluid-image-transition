39a0fc07e31f4855745a99d579e76a352880dc8d
Piotr <piotr.bania@europe.com>
2022-07-26 11:38:59 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import './styles/main.scss'
 import * as THREE from 'three'
+import {
+    OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
 import sushi from './assets/sushi.jpg'
+
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.getElementById('canvas')
 
 // Scene
 const scene = new THREE.Scene()
-
-// Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
 // Textures
 const textureLoader = new THREE.TextureLoader()
@@ -16,22 +24,82 @@ const geometry = new THREE.PlaneBufferGeometry(1, 1, 32, 32)
 
 // Material
 const material = new THREE.RawShaderMaterial({
-    vertexShader: '',
-    fragmentShader: ''
+    vertexShader: `
+        uniform mat4 projectionMatrix;
+        uniform mat4 viewMatrix;
+        uniform mat4 modelMatrix;
+
+        attribute vec3 position;
+
+        void main()
+        {
+            gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+        }
+    `,
+    fragmentShader: `
+        precision mediump float;
+
+        void main()
+        {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    `
 })
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0.25, -0.25, 1)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
 // Renderer
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 // Animate
-function animate() {
-    requestAnimationFrame(animate)
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
     renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
-animate()
+
+tick()
